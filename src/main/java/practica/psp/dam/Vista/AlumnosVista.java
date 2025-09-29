60e70e7c8ff24cb50bcbe0b9170f4ffb3d48ee33
JiaJun <12jjicm21@gmail.com>
2025-09-29 04:20:35 +0200

    Cambios previstos futuros

--- src/main/java/practica/psp/dam/Vista/AlumnosVista.java
+++ src/main/java/practica/psp/dam/Vista/AlumnosVista.java
@@ -20,37 +20,37 @@ public class AlumnosVista implements VistaInterfaz{
             %d - Salir.
             """.formatted(OPCION_CREAR_ALUMNO, OPCION_LISTAR_ALUMNO, OPCION_SALIR);
 
-        System.out.println(opciones);
+        System.out.println(opciones);    //Manejar el rango a 1-3 con while
 
         sc = new Scanner(System.in);
-        int opcion = Integer.parseInt(sc.nextLine());
+        int opcion = Integer.parseInt(sc.nextLine());   //Excepción NumberFormatException
 
         return opcion;
     }
 
     @Override
-    public String[] crearAlumnos() {
+    public String[] crearAlumnos() {    //Cambiar a Alumno crearAlumnos() para que retorne un objeto alumno directamente
         String[] datosAlumno = new String[ATRIBUTOS_ALUMNO];
 
         sc = new Scanner(System.in);
         System.out.println("Nombre del alumno: ");
-        datosAlumno[0] = sc.nextLine();
+        datosAlumno[0] = sc.nextLine();     //Mantenimiento del proyecto, variables
         System.out.println("Apellidos del alumno: ");
         datosAlumno[1] = sc.nextLine();
-        System.out.println("Edad del alumno: ");
+        System.out.println("Edad del alumno: ");    //Manejar la edad, debe ser un entero positivo
         datosAlumno[2] = sc.nextLine();
 
         return datosAlumno;
     }
 
     @Override
-    public void listarAlumnos(String[] datos) {
+    public void listarAlumnos(String[] datos) {     //Cambiar a void listarAlumnos(Alumnos alumno) para usar los getters de Alumno
         String mostrarDatos = """
             ALUMNO
             Nombre: %s
             Apellidos: %s
             Edad: %s
-            """.formatted(datos[0], datos[1], datos[2]);
+            """.formatted(datos[0], datos[1], datos[2]);    //Mantenimiento del proyecto, variables
         System.out.println(mostrarDatos);
     }
 }
